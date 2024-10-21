8dc66664a4f16d624ecacc39d42dc48e76353cba
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-10-21 17:58:44 +0530

    add the code

--- src/helper/fileUpload.js
+++ src/helper/fileUpload.js
@@ -3,7 +3,7 @@ const multerS3 = require('multer-s3');
 const { S3Client } = require('@aws-sdk/client-s3');
 
 const s3 = new S3Client({
-    region: "eu-north-1", // Verify this matches your S3 bucket's region
+    region:process.env.region , // Verify this matches your S3 bucket's region
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
@@ -11,13 +11,11 @@ const s3 = new S3Client({
 });
 
 
-console.log("Access Key:", process.env.AWS_ACCESS_KEY_ID);
-console.log("Secret Key:", process.env.AWS_SECRET_ACCESS_KEY);
 
 const getMulterStorage = (storagePath) => {
     const s3Storage = multerS3({
         s3: s3,
-        bucket: "cell121",
+        bucket: process.env.bucket,
         metadata: (req, file, cb) => {
             cb(null, { fieldname: file.fieldname });
         },
