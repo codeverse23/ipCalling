0ed04544c7417452004982b34000c65acd1507a1
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-11-12 15:45:44 +0530

    add the code

--- src/router/adminRouter.js
+++ src/router/adminRouter.js
@@ -1,80 +1,252 @@
 const express = require("express");
-const userRouter = express.Router();
-const userController = require("../controller/userController");
+const adminRouter = express.Router();
+const adminController = require("../controller/adminController");
+const { varifyToken } = require("../helper/varifyTokenFn");
+const multer = require("multer");
+const path = require("path");
 const verifyRoles = require("../../middlewares/verifyRoles");
 const roleList = require("../../src/consts/autho");
-const { varifyToken } = require("../helper/varifyTokenFn");
 const getMulterStorage = require("../helper/fileUpload");
-const uploadSingle = getMulterStorage("profileImage").single("profileImage");
-
-userRouter.post("/signUp", userController.signUp);
-userRouter.post("/checkUsername", userController.checkUsername);
-userRouter.post("/varifyOtp", userController.varifyOtp);
-userRouter.post("/sendOtp", userController.forgotPasswordSendOtp);
-userRouter.put("/forgotChangePassword", userController.forgotChangePassword);
-userRouter.post("/login", userController.login);
-userRouter.put(
-  "/changPassword",
+const uploadSingle = getMulterStorage("groupImage").single("groupImage");
+adminRouter.post("/login", adminController.login);
+adminRouter.get("/userList", varifyToken, adminController.userList);
+adminRouter.delete("/deleteUser", varifyToken, adminController.deleteUser);
+adminRouter.patch(
+  "/blockUnblockUser",
   varifyToken,
-  verifyRoles(roleList.USER),
-  userController.changPassword
+  adminController.blockUnblockUser
+);
+adminRouter.get(
+  "/adminProfile",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.adminProfile
 );
 
-userRouter.put(
+adminRouter.get(
+  "/totalActiveUser",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.totalActiveUser
+);
+
+adminRouter.get(
+  "/totalDeactiveUser",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.totalDeactiveUser
+);
+
+adminRouter.get(
+  "/totalPendingReq",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.totalPendingReq
+);
+
+adminRouter.get(
+  "/blockUserList",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.blockUserList
+);
+
+adminRouter.get(
+  "/deshboardCount",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.deshboardCount
+);
+
+adminRouter.post("/SendOtp", adminController.forgotPasswordSendOtp);
+adminRouter.post("/varifyOtp", adminController.varifyOtp);
+
+adminRouter.post("/addUser", varifyToken, adminController.addUser);
+
+adminRouter.post(
+  "/addNotification",
+  varifyToken,
+  adminController.addNotification
+);
+adminRouter.put("/addPermissions", varifyToken, adminController.addPermissions);
+adminRouter.put(
+  "/removePermissions",
+  varifyToken,
+  adminController.removePermissions
+);
+adminRouter.put(
+  "/changSubAdminPassword",
+  varifyToken,
+  adminController.changSubAdminPassword
+);
+adminRouter.put(
+  "/changSubAdminPassword",
+  varifyToken,
+  adminController.changSubAdminPassword
+);
+adminRouter.put(
+  "/updateSubAdminProfile",
+  varifyToken,
+  adminController.updateSubAdminProfile
+);
+adminRouter.get(
+  "/getSubAdminPermissions",
+  varifyToken,
+  adminController.getSubAdminPermissions
+);
+adminRouter.post("/addRole", varifyToken, adminController.addRole);
+
+adminRouter.put(
+  "/updateNotification",
+  varifyToken,
+  adminController.updateNotification
+);
+
+adminRouter.delete(
+  "/deleteNotification",
+  varifyToken,
+  adminController.deleteNotification
+);
+
+adminRouter.put(
+  "/updateAdminProfile",
+  varifyToken,
+  uploadSingle, // Corrected from upload.single("file") to uploadSingle
+  adminController.updateAdminProfile
+);
+
+adminRouter.get(
+  "/notificationList",
+  varifyToken,
+  adminController.notificationList
+);
+adminRouter.post("/addSystemInfo", varifyToken, adminController.addSystemInfo);
+adminRouter.get("/systemInfoList", varifyToken, adminController.systemInfoList);
+
+adminRouter.delete(
+  "/deleteSytemInfo",
+  varifyToken,
+  adminController.deleteSytemInfo
+);
+
+adminRouter.post(
+  "/addprivecyPolicy",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.addprivecyPolicy
+);
+
+adminRouter.put(
+  "/updatePrivecyPolicy",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.updatePrivecyPolicy
+);
+
+adminRouter.post(
+  "/addtermCondition",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.addtermCondition
+);
+
+adminRouter.put(
+  "/updateTermCondition",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.addtermCondition
+);
+
+adminRouter.get(
+  "/deshboardCount",
+  varifyToken,
+  adminController.deshboardCount
+);
+
+adminRouter.post(
+  "/addQusAns",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.addQusAns
+);
+
+adminRouter.put(
+  "/updateQusAns",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.updateQusAns
+);
+
+adminRouter.delete("/deleteQusAns", varifyToken, adminController.deleteQusAns);
+
+adminRouter.post(
+  "/groupList",
+  varifyToken,
+  uploadSingle,
+  adminController.groupList
+);
+
+adminRouter.post(
+  "/createGroup",
+  varifyToken,
+  uploadSingle,
+  adminController.createGroup
+);
+
+adminRouter.post("/addMembers", varifyToken, adminController.addMembers);
+
+adminRouter.post("/getGroupInfo", varifyToken, adminController.getGroupInfo);
+
+adminRouter.delete("/deleteGroup", varifyToken, adminController.deleteGroup);
+
+adminRouter.delete(
+  "/deleteUserGroup",
+  varifyToken,
+  adminController.deleteUserGroup
+);
+
+adminRouter.put(
+  "/userAcceptReject",
+  varifyToken,
+  verifyRoles(roleList.ADMIN),
+  adminController.userAcceptReject
+);
+
+adminRouter.put(
   "/changEmail",
   varifyToken,
-  verifyRoles(roleList.USER),
-  userController.changEmail
+  verifyRoles(roleList.ADMIN),
+  adminController.changEmail
 );
 
-userRouter.put(
-  "/updateUsername",
+adminRouter.put(
+  "/changName",
   varifyToken,
-  userController.updateUsername
+  verifyRoles(roleList.ADMIN),
+  adminController.changName
 );
 
-userRouter.post(
-  "/privacyPolicyList",
+adminRouter.put(
+  "/changPassword",
   varifyToken,
-  userController.privacyPolicyList
+  verifyRoles(roleList.ADMIN),
+  adminController.changPassword
 );
 
-userRouter.post(
-  "/termConditionList",
-  varifyToken,
-  userController.termConditionList
-);
-
-userRouter.post(
+adminRouter.post(
   "/privecyInfo",
   varifyToken,
-  userController.privecyInfo
+  verifyRoles(roleList.ADMIN),
+  adminController.privecyInfo
 );
 
-userRouter.put(
+
+
+
+adminRouter.put(
   "/updatePrivecyInfo",
   varifyToken,
-  userController.updatePrivecyInfo
+  verifyRoles(roleList.ADMIN),
+  adminController.updatePrivecyInfo
 );
-
-userRouter.post(
-  "/genrateToken",
-  userController.genrateToken
-);
-
-userRouter.get(
-  "/getProfile",
-  varifyToken,
-  userController.getProfile
-);
-
-userRouter.put(
-  "/updateProfileImage",
-  varifyToken,   // Add token verification here as well
-  uploadSingle,   // multer upload middleware
-  userController.updateProfileImage
-);
-
-userRouter.get("/userList", varifyToken, userController.userList);
-
-module.exports = userRouter;
+module.exports = adminRouter;
